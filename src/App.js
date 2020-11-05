ef2c4da4b652208e4afa6db83cfefbdf6c8f9269
jyntn23 <jayantonroblico23@gmail.com>
2020-11-05 23:38:53 +0800

    PH cases data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import './coronavirus.png'
 
 const App = () => {
 
@@ -27,71 +28,77 @@ const App = () => {
     const response = await fetch(url)
     const data = await response.json();
     console.log(data)
-    setToday(data.todayCases)
-    setDeath(data.todayDeaths)
-    setTests(data.tests)
-    setConfirmed(data.cases)
-    setRecovered(data.recovered)
-    setAtdeaths(data.deaths)
-    setActive(data.active)
+    setToday(data.todayCases.toLocaleString('en'))
+    setDeath(data.todayDeaths.toLocaleString('en'))
+    setTests(data.tests.toLocaleString('en'))
+    setConfirmed(data.cases.toLocaleString('en'))
+    setRecovered(data.recovered.toLocaleString('en'))
+    setAtdeaths(data.deaths.toLocaleString('en'))
+    setActive(data.active.toLocaleString('en'))
   }
 
   return (
     <div className="App">
       <header>
-        <h1>Philipines Covid19 Cases Tracker</h1>
+        <h1>Philipines Co<span><img src="coronavirus.png" alt=""/></span>vid19 Cases Tracker</h1>
       </header>
 
       <section id="cases">
-        <div className="ph-section">
+        <div className="cases-wrapper">
+          <div className="ph-section">
 
-          <div className="today">
-            <h2>Today {date}</h2>
-            <div className="today-container">
-              <div className="today-confirm">
-                <h4>CONFIRMED CASES</h4>
-                <h1>{today}</h1>
-              </div>
-              <div className="today-deaths">
-                <h4>DEATHS</h4>
-                <h1>{death}</h1>
+            <div className="today">
+              <h2>Today</h2>
+              <p>{date}</p>
+              <div className="box-container">
+                <div className="box">
+                  <h4>CONFIRMED CASES</h4>
+                  <h1>{today}</h1>
+                </div>
+                <div className="box">
+                  <h4>DEATHS</h4>
+                  <h1>{death}</h1>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="all-time">
-            <h2>All Time since day 1 of confirmed cases</h2>
-            <div className="all-time-container">
-              <div className="all-time-test">
-                <h4>TESTS CONDUCTED</h4>
-                <h1>{tests}</h1>
-              </div>
-              <div className="all-time-confirmed">
-                <h4>CONFIRMED CASES</h4>
-                <h1>{confirmed}</h1>
-              </div>
-              <div className="all-time-recovered">
-                <h4>RECOVERED</h4>
-                <h1>{recovered}</h1>
-              </div>
-              <div className="all-time-deaths">
-                <h4>DEATHS</h4>
-                <h1>{atDeaths}</h1>
-              </div>
-              <div className="all-time-active">
-                <h4>ACTIVE CASES</h4>
-                <h1>{active}</h1>
+            <div className="all-time">
+              <h2>All Time</h2>
+              <p>since day 1 of confirmed cases</p>
+              <div className="box-container">
+                <div className="box">
+                  <h4>TESTS CONDUCTED</h4>
+                  <h1>{tests}</h1>
+                </div>
+                <div className="box">
+                  <h4>CONFIRMED CASES</h4>
+                  <h1>{confirmed}</h1>
+                </div>
+                <div className="box">
+                  <h4>RECOVERED</h4>
+                  <h1>{recovered}</h1>
+                </div>
+                <div className="box">
+                  <h4>DEATHS</h4>
+                  <h1>{atDeaths}</h1>
+                </div>
+                <div className="box">
+                  <h4>ACTIVE CASES</h4>
+                  <h1>{active}</h1>
+                </div>
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
-
       </section>
 
 
 
-      <footer>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></footer>
+      <footer>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+        <p>&#60;/&#62; by Jay Anton Roblico</p>
+      </footer>
     </div>
   );
 }
