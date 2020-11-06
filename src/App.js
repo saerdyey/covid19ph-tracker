0ee62f1f65c5a754f315124f5102c032928706b9
jyntn23 <jayantonroblico23@gmail.com>
2020-11-06 09:41:42 +0800

    global data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import './coronavirus.png'
+import coronavirus from './coronavirus.png'
 
 const App = () => {
 
-  const url = 'https://disease.sh/v3/covid-19/countries/Philippines?yesterday=true&twoDaysAgo=true&strict=true';
+  const urlPH = 'https://disease.sh/v3/covid-19/countries/Philippines?yesterday=true&twoDaysAgo=true&strict=true';
+  const urlGB = 'https://disease.sh/v3/covid-19/all'
 
   useEffect(() =>{
     getData()
+    getDataGlobal()
   }, [])
 
   var todayD = new Date();
@@ -25,7 +27,7 @@ const App = () => {
   const [active, setActive] = useState()
 
   const getData = async () => {
-    const response = await fetch(url)
+    const response = await fetch(urlPH)
     const data = await response.json();
     console.log(data)
     setToday(data.todayCases.toLocaleString('en'))
@@ -37,16 +39,36 @@ const App = () => {
     setActive(data.active.toLocaleString('en'))
   }
 
+  //? GLOBAL DATA
+  const [Gtests, setGtests] = useState()
+  const [Gcases, setGcases] = useState()
+  const [Grecovered, setGrecovered] = useState()
+  const [Gdeaths, setGdeaths] = useState()
+  const [Gactive, setGactive] = useState()
+  const [Gaffected, setGaffected] = useState()
+
+  const getDataGlobal = async () => {
+    const response = await fetch(urlGB)
+    const globaldata = await response.json();
+    setGtests(globaldata.tests.toLocaleString('en'))
+    setGcases(globaldata.cases.toLocaleString('en'))
+    setGrecovered(globaldata.recovered.toLocaleString('en'))
+    setGdeaths(globaldata.deaths.toLocaleString('en'))
+    setGactive(globaldata.active.toLocaleString('en'))
+    setGaffected(globaldata.affectedCountries.toLocaleString('en'))
+
+  }
+
   return (
     <div className="App">
       <header>
-        <h1>Philipines Co<span><img src="coronavirus.png" alt=""/></span>vid19 Cases Tracker</h1>
+        <h1>PHILIPPINES C<img src={coronavirus} alt="" width="50" id="logo"/>VID19 CASES TRACKER</h1>
       </header>
 
       <section id="cases">
         <div className="cases-wrapper">
           <div className="ph-section">
-
+          <h1 id="PH">PHILIPPINES</h1>
             <div className="today">
               <h2>Today</h2>
               <p>{date}</p>
@@ -90,14 +112,44 @@ const App = () => {
             </div>
 
           </div>
+
+          <div className="global-section">
+          <h1 id="PH">GLOBAL</h1>
+          <div className="box-container">
+                <div className="box">
+                  <h4>TESTS CONDUCTED</h4>
+                  <h1>{Gtests}</h1>
+                </div>
+                <div className="box">
+                  <h4>CONFIRMED CASES</h4>
+                  <h1>{Gcases}</h1>
+                </div>
+                <div className="box">
+                  <h4>RECOVERED</h4>
+                  <h1>{Grecovered}</h1>
+                </div>
+                <div className="box">
+                  <h4>DEATHS</h4>
+                  <h1>{Gdeaths}</h1>
+                </div>
+                <div className="box">
+                  <h4>ACTIVE CASES</h4>
+                  <h1>{Gactive}</h1>
+                </div>
+                <div className="box">
+                  <h4>AFFECTED COUNRIES</h4>
+                  <h1>{Gaffected}</h1>
+                </div>
+              </div>
+          </div>
         </div>
       </section>
 
 
-
       <footer>
-        <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
         <p>&#60;/&#62; by Jay Anton Roblico</p>
+        <p>API by <a href="https://disease.sh/docs/#/COVID-19%3A%20Worldometers/get_v3_covid_19_countries__country_">Novel Covid19 API</a></p>
+        <p>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
       </footer>
     </div>
   );
